7f78f0eac82cf094022af19bfe07667fde207010
Louis <louis.duplessis@env.ethz.ch>
2019-09-30 16:35:58 +0100

    Add and test ESP likelihood

--- R/skyline_lk.R
+++ R/skyline_lk.R
@@ -30,7 +30,7 @@ skyline_lk <- function(tree, popSizes, segmentSizes=NULL, full=TRUE) {
       segmentSizes <- rep(1, tree$Nnode)
   }
 
-  intervals       <- getTreeIntervals(tree)
+  intervals       <- beastio::getTreeIntervals(tree)
   intervals$group <- mapGroups(intervals, segmentSizes, "coalescent")
 
   lk <- 0
@@ -53,19 +53,27 @@ skyline_lk <- function(tree, popSizes, segmentSizes=NULL, full=TRUE) {
 #' Epoch Sampling Skyline Plot log-likelihood
 #'
 #' @export
-esp_lk <- function(tree, segmentSizes, popSizes, epochSizes, sampIntensities) {
+esp_lk <- function(tree, popSizes, segmentSizes=NULL, sampIntensities, epochSizes=NULL, full=TRUE) {
 
-  intervals         <- getTreeIntervals(tree)
+  if (is.null(segmentSizes)) {
+    segmentSizes <- rep(1, tree$Nnode)
+  }
+
+  if (is.null(epochSizes)) {
+    segmentSizes <- rep(1, length(tree$tip.label))
+  }
+
+  intervals         <- beastio::getTreeIntervals(tree)
   intervals$segment <- mapGroups(intervals, segmentSizes, c("coalescent","sample"))
   intervals$epoch   <- mapGroups(intervals, epochSizes, "sample")
 
   lk <- 0
   for (i in 1:nrow(intervals)) {
     alpha <- choose(intervals$nlineages[i],2)
-    beta  <- sampIntensities[intervals$epoch[i]]
+    beta  <- ifelse(intervals$epoch[i] == 0, 0, sampIntensities[intervals$epoch[i]])
     N     <- popSizes[intervals$segment[i]]
 
-    lk    <- lk - intervals$length[i]*(beta*N - alpha/N)
+    lk    <- lk - intervals$length[i]*(beta*N + alpha/N)
     if (intervals$nodetype[i] == "coalescent") {
       lk <- lk - log(N)
       if (full) {
@@ -79,22 +87,3 @@ esp_lk <- function(tree, segmentSizes, popSizes, epochSizes, sampIntensities) {
 
   return(lk)
 }
-
-
-# intervals <- getTreeIntervals(tree)
-# groupSizes <- c(2,2,2,2,3)
-# groupTypes <- c("sample", "coalescent")
-# intervals$group <- mapGroups(intervals, groupSizes, groupTypes)
-#
-# groupSizes <- c(1,1,1,1,2)
-# groupTypes <- c("coalescent")
-# intervals$group <- mapGroups(intervals, groupSizes, groupTypes)
-#
-#
-# groupSizes <- c(1,1,1,1,1,1)
-# groupTypes <- c("sample")
-# intervals$group <- mapGroups(intervals, groupSizes, groupTypes)
-#
-# tree.test <- read.tree(text = "((D4Philip56:30.0,(D4Philip64:23.0,D4Philip84:23.0):7.0):10.0,(D4SLanka78:25.0,(D4Thai78:11.0,D4Thai84:11.0):14.0):15.0);") # load tree
-# sk1 <- skyline(tree.test)
-# cat(sprintf("%.12f", sk1$logL))
--- tests/testthat/testSkylineLikelihoods.R
+++ tests/testthat/testSkylineLikelihoods.R
@@ -3,19 +3,19 @@ context("Generalized Skyline Plot likelihood")
 test_that("skyline_lk() gives the same results as ape::skyline (homochronous trees only)", {
 
   # Classic skyline, small tree (groupSizes = 1)
-  tree.test <- read.tree(text = "((D4Philip56:30.0,(D4Philip64:23.0,D4Philip84:23.0):7.0):10.0,(D4SLanka78:25.0,(D4Thai78:11.0,D4Thai84:11.0):14.0):15.0);") # load tree
-  sk1 <- skyline(tree.test)
+  tree.test <- ape::read.tree(text = "((D4Philip56:30.0,(D4Philip64:23.0,D4Philip84:23.0):7.0):10.0,(D4SLanka78:25.0,(D4Thai78:11.0,D4Thai84:11.0):14.0):15.0);") # load tree
+  sk1 <- ape::skyline(tree.test)
   expect_equal(skyline_lk(tree.test, sk1$population.size), sk1$logL)
 
   # Classic skyline, big tree (groupSizes = 1)
-  data("hivtree.newick") # example tree in NH format
-  tree.hiv <- read.tree(text = hivtree.newick) # load tree
-  sk1      <- skyline(tree.hiv)
+  data("hivtree.newick", package="ape") # example tree in NH format
+  tree.hiv <- ape::read.tree(text = hivtree.newick) # load tree
+  sk1      <- ape::skyline(tree.hiv)
   expect_equal(skyline_lk(tree.hiv, sk1$population.size), sk1$logL, tolerance=1e-6)
 
   # Generalized skyline, big tree
-  cl2      <- collapsed.intervals(coalescent.intervals(tree.hiv),0.0119)
-  sk2      <- skyline(cl2)
+  cl2      <- ape::collapsed.intervals(ape::coalescent.intervals(tree.hiv),0.0119)
+  sk2      <- ape::skyline(cl2)
   groupSizes <- c()
   for (i in 1:max(cl2$collapsed.interval)) {
     groupSizes <- c(groupSizes, sum(cl2$collapsed.interval == i))
@@ -33,5 +33,12 @@ test_that("skyline_lk() gives the same results as the BSP", {
 })
 
 
+test_that("esp_lk() gives the same results as those calculated by hand (and by BESP)", {
+  expect_equal(esp_lk(ape::read.tree("../testTree1.tree"), c(1,2), c(8,3), 1, 6, full=FALSE), -305.57944154167984)
+  expect_equal(esp_lk(ape::read.tree("../testTree2.tree"), c(1,2), c(6,3), 2, 5, full=FALSE), -56.2274112777602)
+  expect_equal(esp_lk(ape::read.tree("../testTree3.tree"), c(2,3), c(6,5), c(1,2,3), c(3,2,1), full=FALSE), -183.8716748273099)
+  expect_equal(esp_lk(ape::read.tree("../testTree4.tree"), c(3,2,1), c(3,4,4), c(2,3), c(4,2), full=FALSE), -205.234349834419)
+  expect_equal(esp_lk(ape::read.tree("../testTree5.tree"), c(2,3,4), c(5,2,4), c(2,3,4), c(3,2,1), full=FALSE), -289.280186700424)
+})
 
 
